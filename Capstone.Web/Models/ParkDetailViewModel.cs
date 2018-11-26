60448bca25a5d1d0a1847fbd85b697a90a6efedc
Anthony Slack <anthonyslack1@gmail.com>
2018-11-26 11:51:27 -0500

    first commit

---
+++ Capstone.Web/Models/ParkDetailViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    /// <summary>
+    /// Holds information needed in the ParkDetails View
+    /// </summary>
+    public class ParkDetailViewModel
+    {
+        /// <summary>
+        /// Holds Park Information
+        /// </summary>
+        public Park ParkDetails = new Park();
+        /// <summary>
+        /// Holds Weather information
+        /// </summary>
+        public Forecast Forecast = new Forecast();
+        /// <summary>
+        /// Holds the char to represent displaying fahrenheit or celsius
+        /// </summary>
+        public char TempType;
+        /// <summary>
+        /// Dictionary to hold extra advice for each weather condition
+        /// </summary>
+        public Dictionary<string, string> Advice = new Dictionary<string, string>()
+        {
+            {"snow","Don't forget to pack snowshoes!" },
+            {"rain","Pack rain gear and wear waterproof shoes." },
+            {"thunderstorms", "Seek shelter! Avoid hiking on exposed ridges." },
+            {"sunny", "Pack sunblock!" },
+            {"hot", "Bring an extra gallon of water." },
+            {"difference", "Wear breathable layers."  },
+            {"cold", "Exposure to frigid temperatures may cause injury or death, stay safe!" }
+        };
+
+        /// <summary>
+        /// Converts Fahrenheit temperatures from the DB to Celsius
+        /// </summary>
+        public void ConvertTemp()
+        {
+            if (TempType == 'C')
+            {
+                foreach(var model in Forecast.FiveDayForecast)
+                {
+                    model.High = (int)Math.Round((model.High - 32) * (5.0 / 9.0));
+                    model.Low = (int)Math.Round((model.Low - 32) * (5.0 / 9.0));
+                }
+            }
+        }
+    }
+}
